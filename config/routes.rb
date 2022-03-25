e5719941d6673badda75efb3db1dedad80285104
issy-yu <c12isiwata@icloud.com>
2022-03-25 14:13:32 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+  root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :comments, only: :create 
+  resources :users, only: :show
+end
